fce0545dd0fd3160c4799b25d8b92f5505ad8011
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-12-15 23:09:05 +0100

    complete app

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import cloudy from "./cloudy.png"
+import rainy from "./rainy.png"
+import storm from "./storm.png"
+import sun from "./sun.png"
+import locationIcon from "./ic_location.png"
+
+
+export {
+    cloudy,
+    rainy,
+    storm,
+    sun,
+    locationIcon,
+}
---
+++ src/data/weatherData.js
@@ -0,0 +1,12 @@
+export const weatherData = {
+    location: "Stockholm, Sweden",
+    day: "Tuesday 19:00",
+    temperature: "-12",
+    metrics: [
+        {name: "Precipitation", value: "2%"},
+        {name: "Humidity", value: "81%"},
+        {name: "Wind", value: "6 km/h"},
+        ]
+    ,
+    cloud: "Partly Cloudy"
+}
